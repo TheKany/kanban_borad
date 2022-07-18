d2508791ad0b61bf535a929b7c3ed322a45a96b5
TheKany <skarnjs06@naver.com>
2022-07-18 13:54:30 +0900

    오타 수정

--- js/Clock.js
+++ js/Clock.js
@@ -15,7 +15,7 @@ function getDate() {
   const date = new Date();
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
-  const day = date.getDay();
+  const day = date.getDate();
 
   const StrMonth = getMonthString(month);
 
